1688ff48fc971dd3cca25b4f3489c2517b600bce
Eugene Dyko <yevdyko@gmail.com>
2021-03-12 15:23:36 +0100

    Add sketch as an example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import P5Wrapper from 'react-p5-wrapper';
 
 function App() {
+  let sketch = (p5) => {
+    let yoff = 0.0;
+
+    p5.setup = () => {
+      p5.createCanvas(710, 400);
+    }
+
+    p5.draw = () => {
+      p5.background(51);
+      p5.fill(255);
+      p5.beginShape();
+      let xoff = 0;
+
+      for (let x = 0; x <= p5.width; x += 10) {
+        let y = p5.map(p5.noise(xoff, yoff), 0, 1, 200, 300);
+        p5.vertex(x, y);
+        xoff += 0.05;
+      }
+
+      yoff += 0.01;
+      p5.vertex(p5.width, p5.height);
+      p5.vertex(0, p5.height);
+      p5.endShape(p5.CLOSE);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <P5Wrapper sketch={sketch} />
   );
 }
 
